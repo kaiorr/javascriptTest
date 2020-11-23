684d25930b75b013320253b5e4035c03c78c0f87
kaiorr <kaio.rocharibeiro@gmail.com>
2020-11-23 09:51:13 -0300

    feat(object !array.isarray): created validation in the query query string when an object is params
    created validation in the query query string when an object is passed as a parameter. Thus leaving
    with an additional validation in the implemented test, preventing it from being broken.

--- initial/src/lib/queryString.js
+++ initial/src/lib/queryString.js
@@ -1,1 +1,6 @@
-module.exports.queryString = (obj) => Object.entries(obj).map(([key, value]) => `${key}=${value}`).join('&')
+module.exports.queryString = (obj) => Object.entries(obj).map(([key, value]) => {
+  if(typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your params')
+  }
+  return `${key}=${value}`
+}).join('&')
--- initial/src/lib/queryString.spec.js
+++ initial/src/lib/queryString.spec.js
@@ -7,8 +7,29 @@ describe('Object to query string', () => {
       profession: 'Developer'
     }
 
-    expect(queryString(obj)).toBe(
-      'name=Kaio&profession=Developer'
-    )
-  });
+    expect(queryString(obj)).toBe('name=Kaio&profession=Developer')});
+
+  it('should create a valid query string even when an array', () => {
+    const obj = {
+      name: 'Kaio',
+      abilities: ['JS', 'TDD']
+    }
+
+    expect(queryString(obj)).toBe('name=Kaio&abilities=JS,TDD')
+  })
+
+  it('should create a valid query string even when an array', () => {
+    const obj = {
+      name: 'Kaio',
+      abilities: {
+        first: 'JS',
+        second: 'TDD'
+      },
+    }
+
+    expect(() => {
+      queryString(obj)
+    }).toThrowError();
+  })
+
 })
