9a055a9a49e6b9683ab038c431244849f7f80519
kaiorr <kaio.rocharibeiro@gmail.com>
2020-11-27 11:07:53 -0300

    feat: initial commit for front-end testing
    initial commit for front-end testing

---
+++ frontend-test-application/pages/index.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+
+    </div>
+  )
+}
